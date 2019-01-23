aa06729aa7dd8e424c86deda9fef5c71fd8725b4
Artemiy Rodionov <wertins71@gmail.com>
2019-01-23 17:41:13 +0300

    Remove excess deps. Add description to cli

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use clap::{Arg, App};
 fn app<'a, 'b>() -> App<'a, 'b> {
     App::new("yota")
         .version(clap::crate_version!())
+        .about("Manage an user account of Yota provider")
         .author("Artemiy Rodionov <wertins71@gmail.com>")
         .global_settings(&[
             clap::AppSettings::ColoredHelp,
